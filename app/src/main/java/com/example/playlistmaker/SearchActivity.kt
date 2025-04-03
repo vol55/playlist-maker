9875af922e65d73848831b50960d7ebd5c6ce383
vol55 <akpx99@gmail.com>
2025-04-04 03:24:53 +0500

    дз 4

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -13,7 +13,9 @@ import androidx.core.view.WindowInsetsCompat
 
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var searchFieldValue: String
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -28,13 +30,20 @@ class SearchActivity : AppCompatActivity() {
         val textWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchFieldValue = s.toString()
                 clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
             }
 
             override fun afterTextChanged(s: Editable?) {}
         }
 
+        if (savedInstanceState != null) {
+            searchFieldValue = savedInstanceState.getString(SEARCH_FIELD_VALUE, "")
+            searchInput.setText(searchFieldValue)
+        }
+
         searchInput.addTextChangedListener(textWatcher)
+
         clearButton.setOnClickListener {
             searchInput.setText("")
             hideKeyboard(searchInput)
@@ -44,5 +53,14 @@ class SearchActivity : AppCompatActivity() {
     private fun hideKeyboard(view: View) {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_FIELD_VALUE, searchFieldValue)
+    }
+
+    companion object {
+        private const val SEARCH_FIELD_VALUE = "SEARCH_STRING"
     }
 }
