07dd26117784439f901e2474c14e8a0d39abc76f
vol55 <akpx99@gmail.com>
2025-05-02 21:00:15 +0500

    sprint 11

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,21 +1,43 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.appbar.MaterialToolbar
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class SearchActivity : AppCompatActivity() {
+    private val iTunesBaseUrl = " https://itunes.apple.com"
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(iTunesBaseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    private val iTunesService = retrofit.create(ITunesApi::class.java)
+    private val tracks = ArrayList<Track>()
+    private val trackAdapter = TrackAdapter(tracks)
+
+    private lateinit var noNetworkView: LinearLayout
+    private lateinit var noResultsView: LinearLayout
+
     private lateinit var searchFieldValue: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -25,52 +47,20 @@ class SearchActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        noNetworkView = findViewById<LinearLayout>(R.id.llNoNetwork)
+        noResultsView = findViewById<LinearLayout>(R.id.llNoResults)
 
-        val trackAdapter = TrackAdapter(
-            listOf(
-                Track(
-                    trackName = "Smells Like Teen Spirit",
-                    artistName = "Nirvana",
-                    trackTime = "5:01",
-                    artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-                ),
-                Track(
-                    trackName = "Billie Jean",
-                    artistName = "Michael Jackson",
-                    trackTime = "4:35",
-                    artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-                ),
-                Track(
-                    trackName = "Stayin' Alive",
-                    artistName = "Bee Gees",
-                    trackTime = "4:10",
-                    artworkUrl100 = "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-                ),
-                Track(
-                    trackName = "Whole Lotta Love",
-                    artistName = "Led Zeppelin",
-                    trackTime = "5:33",
-                    artworkUrl100 = "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-                ),
-                Track(
-                    trackName = "Sweet Child O'Mine",
-                    artistName = "Guns N' Roses",
-                    trackTime = "5:03",
-                    artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-                )
-            )
-        )
         val rvTrackList = findViewById<RecyclerView>(R.id.rvTrackList)
         rvTrackList.adapter = trackAdapter
-
 
         val toolbar = findViewById<MaterialToolbar>(R.id.search_activity_toolbar)
         toolbar.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
 
-        val searchInput = findViewById<EditText>(R.id.input_field)
         val clearButton = findViewById<ImageView>(R.id.clear_button)
+
+        val searchInput = findViewById<EditText>(R.id.input_field)
 
         val textWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
@@ -87,12 +77,71 @@ class SearchActivity : AppCompatActivity() {
             searchInput.setText(searchFieldValue)
         }
 
+        val retryButton = findViewById<Button>(R.id.bvNoNetworkRetry)
+        retryButton.setOnClickListener { showSearchResults(searchInput.text.toString()) }
+
         searchInput.addTextChangedListener(textWatcher)
+        searchInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                showSearchResults(searchInput.text.toString())
+                true
+            }
+            false
+        }
 
         clearButton.setOnClickListener {
             searchInput.setText("")
             hideKeyboard(searchInput)
+            tracks.clear()
+            trackAdapter.notifyDataSetChanged()
+            noNetworkView.visibility = View.GONE
+            noResultsView.visibility = View.GONE
         }
+    }
+
+    private fun showSearchResults(searchQuery: String) {
+        if (searchQuery.isEmpty()) {
+            return
+        }
+        tracks.clear()
+        noNetworkView.visibility = View.GONE
+        noResultsView.visibility = View.GONE
+
+        iTunesService.search(searchQuery)
+            .enqueue(object : Callback<TracksResponse> {
+                @SuppressLint("NotifyDataSetChanged")
+                override fun onResponse(
+                    call: Call<TracksResponse>,
+                    response: Response<TracksResponse>
+                ) {
+                    tracks.clear()
+                    if (response.code() == 200) {
+                        val results = response.body()?.results
+                        if (!results.isNullOrEmpty()) {
+                            tracks.addAll(
+                                results.map { track ->
+                                    Track(
+                                        track.trackName,
+                                        track.artistName,
+                                        track.trackTimeMillis,
+                                        track.artworkUrl100
+                                    )
+                                }
+                            )
+                        } else {
+                            noResultsView.visibility = View.VISIBLE
+                        }
+                        trackAdapter.notifyDataSetChanged()
+                    }
+                }
+
+                @SuppressLint("NotifyDataSetChanged")
+                override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
+                    tracks.clear()
+                    trackAdapter.notifyDataSetChanged()
+                    noNetworkView.visibility = View.VISIBLE
+                }
+            })
     }
 
     private fun hideKeyboard(view: View) {
