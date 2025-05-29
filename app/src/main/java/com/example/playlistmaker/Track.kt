4abb0ab6a1642a68ff0ca7c8682525763bfb35c3
vol55 <akpx99@gmail.com>
2025-05-30 00:25:35 +0500

    add data fields and cover artwork function

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -6,4 +6,10 @@ data class Track(
     val artistName: String,
     val trackTimeMillis: Int,
     val artworkUrl100: String,
-)
+    val collectionName: String,
+    val releaseDate: String,
+    val primaryGenreName: String,
+    val country: String,
+) {
+    fun getCoverArtwork(): String = artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
+}
