f02cc0d56680850b19df3ae97d8b001d73e384a3
vol55 <akpx99@gmail.com>
2025-05-30 00:29:05 +0500

    code refactor

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -3,7 +3,6 @@ package com.example.playlistmaker
 import android.content.Context
 import android.icu.text.SimpleDateFormat
 import android.os.Bundle
-import android.util.Log
 import android.util.TypedValue
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
