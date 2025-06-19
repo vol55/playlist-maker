3d14cfdc9d12350b1f5dabc66cab99f162ffa0cf
vol55 <akpx99@gmail.com>
2025-06-20 02:11:49 +0500

    add previewUrl field

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -14,6 +14,7 @@ data class Track(
     val releaseDate: String,
     val primaryGenreName: String,
     val country: String,
+    val previewUrl: String,
 ) : Parcelable {
     fun getCoverArtwork(): String = artworkUrl100.replaceAfterLast('/', "512x512bb.jpg")
 }
