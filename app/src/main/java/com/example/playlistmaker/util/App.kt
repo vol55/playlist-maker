56ca600726b2c6894aa6b430b6df083297d523c2
vol55 <akpx99@gmail.com>
2025-07-23 02:44:47 +0500

    import ThemeInteractor from Creator

--- app/src/main/java/com/example/playlistmaker/util/App.kt
+++ app/src/main/java/com/example/playlistmaker/util/App.kt
@@ -1,10 +1,8 @@
 package com.example.playlistmaker.util
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
-import com.example.playlistmaker.settings.data.ThemeRepositoryImpl
+import com.example.playlistmaker.creator.Creator
 import com.example.playlistmaker.settings.domain.api.ThemeInteractor
-import com.example.playlistmaker.settings.domain.impl.ThemeInteractorImpl
 
 class App : Application() {
 
@@ -13,17 +11,7 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        val prefs = getSharedPreferences("theme_preferences", MODE_PRIVATE)
-        val repository = ThemeRepositoryImpl(prefs)
-
-        themeInteractor = ThemeInteractorImpl(repository) { isDarkThemeEnabled ->
-            val mode = if (isDarkThemeEnabled) {
-                AppCompatDelegate.MODE_NIGHT_YES
-            } else {
-                AppCompatDelegate.MODE_NIGHT_NO
-            }
-            AppCompatDelegate.setDefaultNightMode(mode)
-        }
+        themeInteractor = Creator.provideThemeInteractor(this)
         themeInteractor.switchTheme(themeInteractor.isDarkTheme())
     }
 }
