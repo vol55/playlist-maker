b352f18d6150e8022a236faba154b31e26544a45
vol55 <akpx99@gmail.com>
2025-07-23 21:11:21 +0500

    support EdgeToEdge

--- app/src/main/java/com/example/playlistmaker/search/ui/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/search/ui/SearchActivity.kt
@@ -14,6 +14,8 @@ import android.widget.LinearLayout
 import android.widget.ProgressBar
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.playlistmaker.R
@@ -54,6 +56,12 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         tracksInteractor = Creator.provideTracksInteractor(this)
         historyInteractor = Creator.provideSearchHistoryInteractor(this)
