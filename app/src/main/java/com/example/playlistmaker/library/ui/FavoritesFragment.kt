fcb1a6a9e1a4c1f68615ccc14301b6daf33ba778
vol55 <akpx99@gmail.com>
2025-08-16 14:44:18 +0500

    null _binding in onDestroyView() to prevent memory leak

--- app/src/main/java/com/example/playlistmaker/library/ui/FavoritesFragment.kt
+++ app/src/main/java/com/example/playlistmaker/library/ui/FavoritesFragment.kt
@@ -14,12 +14,18 @@ class FavoritesFragment : Fragment() {
     }
 
     private val favoritesViewModel: FavoritesViewModel by viewModel()
-    private lateinit var binding: FragmentFavoritesBinding
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/playlistmaker/library/ui/PlaylistsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/library/ui/PlaylistsFragment.kt
@@ -14,12 +14,18 @@ class PlaylistsFragment : Fragment() {
     }
 
     private val playlistsViewModel: PlaylistsViewModel by viewModel()
-    private lateinit var binding: FragmentPlaylistsBinding
+    private var _binding: FragmentPlaylistsBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
+        _binding = FragmentPlaylistsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
