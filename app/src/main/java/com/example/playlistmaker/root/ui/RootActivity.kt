a8bbc774f0715e207df64c37e615ced7b5817e4b
vol55 <akpx99@gmail.com>
2025-08-27 14:46:06 +0500

    add BottomNavigationView

--- app/src/main/java/com/example/playlistmaker/root/ui/RootActivity.kt
+++ app/src/main/java/com/example/playlistmaker/root/ui/RootActivity.kt
@@ -1,7 +1,13 @@
 package com.example.playlistmaker.root.ui
 
 import android.os.Bundle
+import android.view.View
+import androidx.activity.addCallback
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
@@ -13,5 +19,23 @@ class RootActivity : AppCompatActivity() {
 
         binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigationView.isVisible = destination.id != R.id.playerFragment
+        }
+
+        onBackPressedDispatcher.addCallback(this) {
+            if (navController.currentDestination?.id == R.id.playerFragment) {
+                if (!navController.popBackStack()) finish()
+            } else {
+                moveTaskToBack(true)
+            }
+        }
     }
 }
