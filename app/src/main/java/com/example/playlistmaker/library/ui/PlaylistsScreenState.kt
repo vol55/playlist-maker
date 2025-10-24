62ad1d2caeb1a5fd08f2b62a3d86682b4e076962
vol55 <akpx99@gmail.com>
2025-10-24 12:33:50 +0500

    change to sealed interface

--- app/src/main/java/com/example/playlistmaker/library/ui/PlaylistsScreenState.kt
+++ app/src/main/java/com/example/playlistmaker/library/ui/PlaylistsScreenState.kt
@@ -2,7 +2,7 @@ package com.example.playlistmaker.library.ui
 
 import com.example.playlistmaker.library.domain.models.Playlist
 
-sealed class PlaylistsScreenState {
-    object Empty : PlaylistsScreenState()
-    data class Content(val playlists: List<Playlist>) : PlaylistsScreenState()
+sealed interface PlaylistsScreenState {
+    object Empty : PlaylistsScreenState
+    data class Content(val playlists: List<Playlist>) : PlaylistsScreenState
 }
