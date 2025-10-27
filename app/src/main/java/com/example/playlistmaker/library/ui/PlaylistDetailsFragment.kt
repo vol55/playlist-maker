dd94ed980dac7a8e7e9299545d0e5f5eb47bd0eb
vol55 <akpx99@gmail.com>
2025-10-27 18:59:26 +0500

    add track click handler in PlaylistDetailsFragment

--- app/src/main/java/com/example/playlistmaker/library/ui/PlaylistDetailsFragment.kt
+++ app/src/main/java/com/example/playlistmaker/library/ui/PlaylistDetailsFragment.kt
@@ -1,17 +1,24 @@
 package com.example.playlistmaker.library.ui
 
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentPlaylistDetailsBinding
+import com.example.playlistmaker.player.ui.PlayerFragment
+import com.example.playlistmaker.search.domain.models.Track
+import com.example.playlistmaker.search.ui.toUi
+import com.example.playlistmaker.utils.debounce
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PlaylistDetailsFragment : Fragment() {
@@ -20,8 +27,9 @@ class PlaylistDetailsFragment : Fragment() {
     private var _binding: FragmentPlaylistDetailsBinding? = null
     private val binding get() = _binding!!
 
-    private val trackList = ArrayList<com.example.playlistmaker.search.domain.models.Track>()
+    private val trackList = ArrayList<Track>()
     private lateinit var trackAdapter: PlaylistDetailsTrackAdapter
+    private lateinit var onTrackClick: (Track) -> Unit
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -30,6 +38,7 @@ class PlaylistDetailsFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -39,7 +48,17 @@ class PlaylistDetailsFragment : Fragment() {
             insets
         }
 
-        trackAdapter = PlaylistDetailsTrackAdapter(trackList) { track -> }
+        onTrackClick = debounce(
+            CLICK_DEBOUNCE_DELAY, viewLifecycleOwner.lifecycleScope, false
+        ) { track ->
+            val args = PlayerFragment.createArgs(track.toUi())
+            findNavController().navigate(
+                R.id.action_playlistDetailsFragment_to_playerFragment, args
+            )
+
+        }
+
+        trackAdapter = PlaylistDetailsTrackAdapter(trackList) { track -> onTrackClick(track) }
         binding.trackList.apply {
             layoutManager = LinearLayoutManager(requireContext())
             adapter = trackAdapter
@@ -75,5 +94,9 @@ class PlaylistDetailsFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private const val CLICK_DEBOUNCE_DELAY = 200L
     }
 }
